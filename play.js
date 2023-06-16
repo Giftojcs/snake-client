dbdc0dc99d549935e89dbd6bd79508e115e2a07f
giftojcs <ojaighog@gmail.com>
2023-06-16 14:56:52 -0600

    Adding input.js and play.js

--- play.js
+++ play.js
@@ -1,5 +1,17 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
+
+const handleUserInput = function (data) {
+  // Process user input here
+  console.log("User input:", data);
+};
+
+// Register handleUserInput as the "data" event callback for stdin
+process.stdin.on("data", handleUserInput);
+
+// Call setupInput and pass the connection object
+setupInput(conn);
 
