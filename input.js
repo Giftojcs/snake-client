e45514df413b6c84b9fe823da3389a2e1894e5f1
giftojcs <ojaighog@gmail.com>
2023-06-16 16:43:47 -0600

    file updated

--- input.js
+++ input.js
@@ -15,16 +15,16 @@ const handleUserInput = function (data) {
     process.exit();
   }
   if (data === 'w') {
-    connection.write('Move: up');
+    connection.write("Move: up");
   }
   if (data === 'a') {
-    connection.write('Move: left');
+    connection.write("Move: left");
   }
   if (data === 's') {
-    connection.write('Move: down');
+    connection.write("Move: down");
   }
   if (data === 'd') {
-    connection.write('Move: right');
+    connection.write("Move: right");
   }
 };
 
--- play.js
+++ play.js
@@ -4,14 +4,5 @@ const { setupInput } = require("./input");
 console.log("Connecting ...");
 const conn = connect();
 
-const handleUserInput = function (data) {
-  // Process user input here
-  console.log("User input:", data);
-};
-
-// Register handleUserInput as the "data" event callback for stdin
-process.stdin.on("data", handleUserInput);
-
-// Call setupInput and pass the connection object
 setupInput(conn);
 
