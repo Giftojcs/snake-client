fc74b9fa2ed34b297785fd8eb14a62ef97794875
giftojcs <ojaighog@gmail.com>
2023-06-16 18:17:20 -0600

    Updating input.js file

--- input.js
+++ input.js
@@ -7,24 +7,17 @@ const setupInput = function (conn) {
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
-  return stdin;
 };
 
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-  if (data === 'w') {
-    connection.write("Move: up");
-  }
-  if (data === 'a') {
-    connection.write("Move: left");
-  }
-  if (data === 's') {
-    connection.write("Move: down");
-  }
-  if (data === 'd') {
-    connection.write("Move: right");
+const handleUserInput = function (key) {
+  if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
